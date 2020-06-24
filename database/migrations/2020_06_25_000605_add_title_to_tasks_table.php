97d176fcb3343cb71eae75209027fedf3d74b36e
shimizu.daiki <s.daiki5216@softbank.ne.jp>
2020-06-24 15:14:01 +0000

    final

--- database/migrations/2020_06_25_000605_add_title_to_tasks_table.php
+++ database/migrations/2020_06_25_000605_add_title_to_tasks_table.php
@@ -26,6 +26,7 @@ class AddTitleToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
+            
             $table->dropColumn('status');
         });
     }
